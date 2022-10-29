f0e48f38471f5085979c5b0ce64fe3c460a24115
VijayalakshmiGanesh <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2022-10-29 13:43:23 +0530

    transition and css done

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
-window.addEventListener("keydown", getCharCode);
+
 
 function getCharCode(keyPressed) {
     var audioElement = document.querySelector(`audio[data-keyvalue="${keyPressed.keyCode}"]`)
+    var divElement = document.querySelector(`.keys[data-keyvalue="${keyPressed.keyCode}"]`)
+
     if (!audioElement) console.log("Press valid key")
     else {
+        divElement.classList.add("keys-press")
         audioElement.currentTime = 0;
-        audioElement.play()
+        audioElement.play();
     }
 }
+
+function removeTransition(event) {
+    if (event.propertyName !== "transform") return;
+    else this.classList.remove("keys-press")
+}
+
+window.addEventListener("keydown", getCharCode);
+var keys = document.querySelectorAll(".keys")
+
+keys.forEach(element => {
+    // console.log(element)
+    element.addEventListener("transitionend", removeTransition)
+});
